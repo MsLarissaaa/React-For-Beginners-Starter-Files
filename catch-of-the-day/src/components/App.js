42afc9f2eb328aa421aebdf124d33280f8790962
Larissa Metz <lmetz@thoughtworks.com>
2017-07-19 14:56:26 -0500

    Complete video 7 - Passing dynamic data with props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,12 +3,13 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+// analogous to HTML tags, you pass information to component tags via props; can name props whatever you want
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
         <Inventory />
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// with props, 'this' refers to the actual component, props is an object with attributes that we can access; can use this.props anywhere inside the render method and it will be made available
 class Header extends React.Component {
   render() {
+    console.log(this);
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
     )
   }
 }
