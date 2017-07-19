7f64ca4f10f73c297523c48d33dcb62e5a913b5c
Larissa Metz <lmetz@thoughtworks.com>
2017-07-19 11:25:24 -0500

    Complete video 3 - writing first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+// import ReactDom from 'react-dom'; - unneccessary in this case because we don't need the whole package.
+import StorePicker from './components/StorePicker'; //need relative path because otherwise Webpack thinks to look in the node modules directory if it is just a string; don't need the .js on the end - assumed.
+
+
+render(<StorePicker/>, document.querySelector('#main'));
