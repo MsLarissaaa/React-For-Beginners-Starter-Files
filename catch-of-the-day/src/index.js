e1f1d65c1c95fad2fa297c57b96e55c5f5093db4
Larissa Metz <lmetz@thoughtworks.com>
2017-07-19 14:21:09 -0500

    Complete videos 5 & 6 - Load CSS into React application and create application layout with components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import ReactDom from 'react-dom'; - unneccessary in this case because we don't need the whole package.
+import './css/style.css';
+import App from './components/App';
+
 import StorePicker from './components/StorePicker'; //need relative path because otherwise Webpack thinks to look in the node modules directory if it is just a string; don't need the .js on the end - assumed.
 
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
