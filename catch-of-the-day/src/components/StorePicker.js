6bb013fc02b4287bcf18b5610bf3d6e55a644459
Larissa Metz <lmetz@thoughtworks.com>
2017-07-19 11:52:18 -0500

    Complete video 4 - Writing HTML with JSX

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello!</p>
+    // return React.createElement('p', {className: 'Testing'}, 'I love you');
+    return (
+      <form className="store-selector">
+        {/* This is how you put a comment inside JSX */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store ➔</button>
+      </form>
+    )
+    // You can only ever return one parent element in JSX, and you have to self-close your tags
   }
 }
 
