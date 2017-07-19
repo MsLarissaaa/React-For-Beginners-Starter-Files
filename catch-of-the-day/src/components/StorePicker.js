6a496c14a696998a55c76cd689bbdc2f2c6a6aca
Larissa Metz <lmetz@thoughtworks.com>
2017-07-19 17:09:12 -0500

    Complete video 11 - Working with React Events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+// one way to allow methods to refer to this within the class; other way is adding .bind(this) to end of method when it's called; third way is to add an arrow function as below in render form
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    // first grab the text from the box
+    console.log(this.storeInput.value);
+    // second we're going to transition from / to /store/:storeId
+  }
+  // ES6 class functions do NOT have commas after them
   render() {
     // return React.createElement('p', {className: 'Testing'}, 'I love you');
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}> {/* This won't work well if you're rendering multiple StorePickers on a page - use other methods described above */}
         {/* This is how you put a comment inside JSX */}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store ➔</button>
       </form>
     )
