0e778a64b77d8c14ea552891921bb2f8eb385b76
Larissa Metz <lmetz@thoughtworks.com>
2017-07-21 15:26:35 -0500

    Complete video 13 - Understanding State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,27 @@ import Inventory from './Inventory';
 
 // analogous to HTML tags, you pass information to component tags via props; can name props whatever you want
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes })
+    // ^same as this.setState({ fishes: fishes}) - ??? ES6
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
